7174be4cf90a44178df9dd1c18c7ce08913b2ee9
jcmbass77 <31079712+jcmbass77@users.noreply.github.com>
2018-03-22 16:17:10 +1000

    commit

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,16 +14,45 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: Boolean = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
     });
-  }
 
+  }
+  onSubmit() {
+    if (this.isNew) {
+      const newLog = {
+        id: this.generatId(),
+        text: this.text,
+        date: new Date()
+
+        // this.logService.addLog(newLog);
+      };
+    } else {
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // this.logService.updateLog(updLog);
+    }
+  }
+  /* tslint:disable:no-bitwise */
+  generatId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+    /* tslint:enable:no-bitwise */
+  }
 }
